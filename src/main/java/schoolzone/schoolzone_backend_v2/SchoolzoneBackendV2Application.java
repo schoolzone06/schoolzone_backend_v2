679a893e75c55d08e54fae399399bdad8c0f1e71
마현우 <mhw35689402@gmail.com>
2023-12-24 22:10:30 +0900

    init

---
+++ src/main/java/schoolzone/schoolzone_backend_v2/SchoolzoneBackendV2Application.java
@@ -0,0 +1,13 @@
+package schoolzone.schoolzone_backend_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolzoneBackendV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolzoneBackendV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/schoolzone/schoolzone_backend_v2/SchoolzoneBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package schoolzone.schoolzone_backend_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolzoneBackendV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
