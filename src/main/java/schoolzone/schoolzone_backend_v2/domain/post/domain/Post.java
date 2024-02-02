65edd456c6ab8aa792c5f8b64dc557d4d71fe496
마현우 <mhw35689402@gmail.com>
2024-02-02 15:13:32 +0900

    feat(post): 게시물 엔티티 정의

---
+++ src/main/java/schoolzone/schoolzone_backend_v2/domain/post/domain/Post.java
@@ -0,0 +1,31 @@
+package schoolzone.schoolzone_backend_v2.domain.post.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import schoolzone.schoolzone_backend_v2.domain.post.domain.enums.Category;
+import schoolzone.schoolzone_backend_v2.global.entity.BaseTimeEntity;
+
+@Builder
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long authorId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 5000)
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
+}
---
+++ src/main/java/schoolzone/schoolzone_backend_v2/domain/post/domain/enums/Category.java
@@ -0,0 +1,10 @@
+package schoolzone.schoolzone_backend_v2.domain.post.domain.enums;
+
+public enum Category {
+    ALL,
+    CSAT,
+    SPORTS,
+    LOVE,
+    GAME,
+    WORK_OUT
+}
