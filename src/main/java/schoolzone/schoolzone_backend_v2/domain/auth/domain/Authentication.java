1bfa226d6511032a5ba416bd8947ab68401625ea
마현우 <mhw35689402@gmail.com>
2024-02-05 16:34:46 +0900

    refactor(etc): base time entity 적용

--- src/main/java/schoolzone/schoolzone_backend_v2/domain/auth/domain/Authentication.java
+++ src/main/java/schoolzone/schoolzone_backend_v2/domain/auth/domain/Authentication.java
@@ -2,13 +2,14 @@ package schoolzone.schoolzone_backend_v2.domain.auth.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import schoolzone.schoolzone_backend_v2.global.entity.BaseTimeEntity;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
-public class Authentication {
+public class Authentication extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/schoolzone/schoolzone_backend_v2/domain/post/like/domain/PostLike.java
+++ src/main/java/schoolzone/schoolzone_backend_v2/domain/post/like/domain/PostLike.java
@@ -2,13 +2,14 @@ package schoolzone.schoolzone_backend_v2.domain.post.like.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import schoolzone.schoolzone_backend_v2.global.entity.BaseTimeEntity;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
-public class PostLike {
+public class PostLike extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
