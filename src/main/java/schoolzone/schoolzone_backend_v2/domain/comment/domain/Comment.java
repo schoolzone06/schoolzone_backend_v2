dd83de26c07981ea9d4aae4e8378dfd65e2c6411
마현우 <mhw35689402@gmail.com>
2024-02-05 13:00:23 +0900

    feat(comment): 댓글 엔티티 정의

---
+++ src/main/java/schoolzone/schoolzone_backend_v2/domain/comment/domain/Comment.java
@@ -0,0 +1,26 @@
+package schoolzone.schoolzone_backend_v2.domain.comment.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import schoolzone.schoolzone_backend_v2.global.entity.BaseTimeEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long postId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String content;
+}
---
+++ src/main/java/schoolzone/schoolzone_backend_v2/domain/comment/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package schoolzone.schoolzone_backend_v2.domain.comment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import schoolzone.schoolzone_backend_v2.domain.comment.domain.Comment;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
