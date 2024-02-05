1a235ea3ca7c5f6642bb5b9040928ebf2eb0083c
마현우 <mhw35689402@gmail.com>
2024-02-05 14:10:38 +0900

    feat(post_like): 게시물 좋아요 엔티티 정의

---
+++ src/main/java/schoolzone/schoolzone_backend_v2/domain/post/like/domain/PostLike.java
@@ -0,0 +1,22 @@
+package schoolzone.schoolzone_backend_v2.domain.post.like.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class PostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long postId;
+
+    @Column(nullable = false)
+    private Long userId;
+}
