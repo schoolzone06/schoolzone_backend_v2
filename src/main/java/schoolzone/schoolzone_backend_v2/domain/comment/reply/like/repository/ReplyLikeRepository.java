de81fcc6187b4c801dff8ede6e96e510a32dd8d5
마현우 <mhw35689402@gmail.com>
2024-02-20 15:22:05 +0900

    feat(reply_like): 대댓글 좋아요 엔티티 구현

---
+++ src/main/java/schoolzone/schoolzone_backend_v2/domain/comment/reply/like/repository/ReplyLikeRepository.java
@@ -0,0 +1,7 @@
+package schoolzone.schoolzone_backend_v2.domain.comment.reply.like.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import schoolzone.schoolzone_backend_v2.domain.comment.reply.like.domain.ReplyLike;
+
+public interface ReplyLikeRepository extends JpaRepository<ReplyLike, Long> {
+}
